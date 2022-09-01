fb023c68fb24257d92eaee1450b111d7526bb728
Marvin Weis <m_weis2005@yahoo.de>
2022-09-01 21:46:38 +0200

    first commit

---
+++ src/main/java/de/nerobuddy/godmode/GodMode.java
@@ -0,0 +1,17 @@
+package de.nerobuddy.godmode;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GodMode extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
